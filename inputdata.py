d8f08d207a6c599d27deccd17f65bbba00a7ee82
Awan <syahrulramadhansr00@gmail.com>
2023-09-15 14:39:15 +0700

    menghilangkan str dan int

--- inputdata.py
+++ inputdata.py
@@ -1,4 +1,4 @@
-nama_user = str(input("Input nama anda: "))
-umur_user = int(input("Input umur anda: "))
+nama_user = input("Input nama anda: ")
+umur_user = input("Input umur anda: ")
 
 print(f"Selamat datang, {nama_user}! Umur anda sekarang adalah {umur_user} ")
